cfd13359b30841fb312ca83a5a5c7794ed6c99a1
0xLil14n <0xLil14n@gmail.com>
2022-01-30 13:10:10 -0500

    adding sign up page

---
+++ components/signup/setUpForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SetUpForm = () => {
+  return <StyledForm>form</StyledForm>;
+};
+const StyledForm = styled.div`
+  margin: 50px;
+`;
+export default SetUpForm;
